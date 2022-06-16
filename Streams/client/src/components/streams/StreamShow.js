9e9cc521661e4b8857c50a68361c97221a0951b6
PrzemyslawTrabuc <czemek17@gmail.com>
2022-06-16 12:01:05 +0200

    fixed player destroying there was a wrong fucntion createRef instead of useRef hook

--- Streams/client/src/components/streams/StreamShow.js
+++ Streams/client/src/components/streams/StreamShow.js
@@ -8,31 +8,30 @@ import 'video.js/dist/video-js.css';
 
 const StreamShow = ({fetchStream, stream}) => {
     const {id} = useParams();
-    const videoRef = useRef();
-    const player = createRef(null);
-    const videoRef2 = useRef();
-    const player2 = createRef(null);
+    // const videoRef = useRef();
+    // const player = createRef(null);
+    const videoRef2 = useRef(null);
+    const playerRef2 = useRef(null);
 
     // const onVideo = useCallback((el) => {
     //     console.log(el)
     //     setVideoEl(el);
     // }, []);
-
-    const newVideo = () =>{
-       // const videoElement = videoRef2.current;
-        if (player2.current || !stream) {
+    const newVideo = () => {
+        if (playerRef2.current || !stream) {
             return;
         }
-        const playerXD = player2.current = videojs(videoRef2.current,{
-            controls: true,
-            autoplay:true,
-            preload:'auto',
-            fluid:true,
-            sources: [{
-                src:'http://localhost:8000/live/1/index.m3u8',
-            }]
+        playerRef2.current = videojs(videoRef2.current, {
+                controls: true,
+                autoplay: true,
+                preload: 'auto',
+                fluid: true,
+                sources: [{
+                    src: 'http://localhost:8000/live/1/index.m3u8',
+                }]
             }
         );
+
     }
 
     useEffect(() => {
@@ -40,29 +39,32 @@ const StreamShow = ({fetchStream, stream}) => {
     }, [])
 
     useEffect(() => {
-        // buildPlayer();
         newVideo();
+    })
 
+    useEffect(() => {
+        const player = playerRef2.current;
         return () => {
-            if(player.current){
-                player.current.destroy();
+            if (player) {
+                player.dispose();
+                playerRef2.current = null;
                 console.log("destroyed player")
             }
         }
-    })
+    }, [playerRef2])
 
-    const buildPlayer = () => {
-        if (player.current || !stream) {
-            return;
-        }
-        player.current = flv.createPlayer({
-            type: 'flv',
-            url: `http://localhost:8000/live/${id}.flv`
-        });
-        player.current.attachMediaElement(videoRef.current)
-        player.current.load();
-        console.log("build")
-    }
+    // const buildPlayer = () => {
+    //     if (player.current || !stream) {
+    //         return;
+    //     }
+    //     player.current = flv.createPlayer({
+    //         type: 'flv',
+    //         url: `http://localhost:8000/live/${id}.flv`
+    //     });
+    //     player.current.attachMediaElement(videoRef.current)
+    //     player.current.load();
+    //     console.log("build")
+    // }
 
     const renderStream = (stream) => {
         if (stream) {
@@ -72,7 +74,8 @@ const StreamShow = ({fetchStream, stream}) => {
                     {/*<video id="video" ref={videoRef} style={{width: '100%'}} controls></video>*/}
                     <h1>{title}</h1>
                     <h5>{description}</h5>
-                    <video style={{marginLeft:'auto', marginRight:'auto'}} className="vjs-matrix video-js" ref={videoRef2}></video>
+                    <video style={{marginLeft: 'auto', marginRight: 'auto'}} className="vjs-matrix video-js"
+                           ref={videoRef2}></video>
                 </div>
             );
         } else {
